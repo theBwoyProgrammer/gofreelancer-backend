69dd6c551fc9d01bd1855556ef3a998f2d500674
Codeaddictjaver <102610715+HatimDev-HE@users.noreply.github.com>
2022-12-05 12:04:54 +0300

    tested reservations

--- spec/models/reservation.rb
+++ spec/models/reservation.rb
@@ -1,21 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-    describe 'association' do
-        it 'belongs to user' do
-            assc = described_class.reflect_on_association(:user)
-            expect(assc.macro).to eq :belongs_to
-        end
+  describe 'association' do
+    it 'belongs to user' do
+      assc = described_class.reflect_on_association(:user)
+      expect(assc.macro).to eq :belongs_to
+    end
 
-        it 'belongs to freelancer' do
-            assc = described_class.reflect_on_association(:freelancer)
-            expect(assc.macro).to eq :belongs_to
-        end
+    it 'belongs to freelancer' do
+      assc = described_class.reflect_on_association(:freelancer)
+      expect(assc.macro).to eq :belongs_to
     end
-    
-    describe 'validation' do
-        it 'has valid attributes' do
-            expect(Reservation.new(appointment_date: '2020-01-01')).to be_valid
-        end
+  end
+
+  describe 'validation' do
+    user = User.create(name: 'test')
+    freelancer = Freelancer.create(name: 'test', photo: 'test', details: 'test', fee: 1, location: 'test')
+
+    it 'has valid attributes' do
+      expect(Reservation.new(user_id: user.id, freelancer_id: freelancer.id,
+                             appointment_date: '2020-01-01')).to be_valid
     end
+  end
 end
