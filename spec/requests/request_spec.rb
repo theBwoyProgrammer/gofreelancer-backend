905b5e7b5bbfed219c123b8db6700a627160ab0d
usbbush <jeanbienvenusb@gmail.com>
2022-12-06 12:14:37 +0200

    Fix rubocop complaints

--- spec/requests/request_spec.rb
+++ spec/requests/request_spec.rb
@@ -47,10 +47,10 @@ RSpec.describe 'Controllers', type: :request do
   describe 'Reservations' do
     describe 'get#all' do
       before do
-        user = FactoryBot.create(:user)
-        freelancer = FactoryBot.create(:freelancer)
+        my_user = FactoryBot.create(:user)
+        my_freelancer = FactoryBot.create(:freelancer)
         10.times do
-          FactoryBot.create(:reservation, user:, freelancer:)
+          FactoryBot.create(:reservation, user: my_user, freelancer: my_freelancer)
         end
         get "/api/v1/users/#{user.id}/reservations"
       end
