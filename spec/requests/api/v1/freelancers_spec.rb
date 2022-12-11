fa411a0d33fa43b1b91572025a001b5bac953885
usbbush <jeanbienvenusb@gmail.com>
2022-12-11 16:17:33 +0200

    Update api documentation

--- spec/requests/api/v1/freelancers_spec.rb
+++ spec/requests/api/v1/freelancers_spec.rb
@@ -1,12 +1,27 @@
 require 'swagger_helper'
-
 RSpec.describe 'api/v1/freelancers', type: :request do
 
   path '/api/v1/freelancers' do
 
     get('list freelancers') do
+      tags 'Freelancers'
+      produces 'application/json'
       response(200, 'successful') do
-
+        schema type: :array,
+               items: {
+                 type: :object,
+                 properties: {
+                   id: { type: :integer },
+                   name: { type: :string, example: Faker::Name.name },
+                   photo: { type: 'string', example: Faker::Internet.url,
+                            pattern: '^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$' },
+                   details: { type: :string, example: Faker::Lorem.paragraph },
+                   fee: { type: 'integer', example: Faker::Number.between(from: 5, to: 100) },
+                   location: { type: 'string', example: Faker::Address.street_address },
+                   created_at: { type: :string, format: :datetime, example: Faker::Date.backward(days: 23) },
+                   updated_at: { type: :string, format: :datetime, example: Faker::Date.backward(days: 23) }
+                 }
+               }
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -19,6 +34,7 @@ RSpec.describe 'api/v1/freelancers', type: :request do
     end
 
     post('create freelancer') do
+      tags 'Create freelancer'
       response(200, 'successful') do
         consumes 'application/json'
         parameter name: :freelancer, in: :body, schema: {
@@ -29,7 +45,7 @@ RSpec.describe 'api/v1/freelancers', type: :request do
                      pattern: '^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$' },
             details: { type: :string, example: Faker::Lorem.paragraph },
             fee: { type: 'integer', example: Faker::Number.between(from: 5, to: 100) },
-            location: { type: 'string', example: Faker::Address.full_address_as_hash(:full_address) }
+            location: { type: 'string', example: Faker::Address.street_address }
           },
           required: %w[name photo details fee location]
         }
@@ -47,11 +63,46 @@ RSpec.describe 'api/v1/freelancers', type: :request do
 
   path '/api/v1/freelancers/{id}' do
     # You'll want to customize the parameter types...
-    parameter name: 'id', in: :path, type: :string, description: 'id'
-
     get('show freelancer') do
+      tags 'Freelancer'
+      produces 'application/json'
+      parameter name: 'id', in: :path, type: :string, description: 'id', default: 1
       response(200, 'successful') do
-        let(:id) { '123' }
+        schema type: :object,
+               properties: {
+                 id: { type: :integer },
+                 name: { type: :string, example: Faker::Name.name },
+                 photo: { type: 'string', example: Faker::Internet.url,
+                          pattern: '^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$' },
+                 details: { type: :string, example: Faker::Lorem.paragraph },
+                 fee: { type: 'integer', example: Faker::Number.between(from: 5, to: 100) },
+                 location: { type: 'string', example: Faker::Address.street_address },
+                 created_at: { type: :string, format: :datetime, example: Faker::Date.backward(days: 23) },
+                 updated_at: { type: :string, format: :datetime, example: Faker::Date.backward(days: 23) }
+               },
+               required: %w[id name photo details fee location]
+        let(:id) { FactoryBot.create(:freelancer).id }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+      response '404', 'freelancer not found' do
+        let(:id) { 'invalid' }
+        run_test!
+      end
+    end
+    delete('delete freelancer') do
+      tags 'Delete Freelancer'
+      produces 'application/json'
+      parameter name: 'id', in: :path, type: :string, description: 'id', default: 0
+      response(200, 'successful') do
+        let(:id) { FactoryBot.create(:freelancer).id }
 
         after do |example|
           example.metadata[:response][:content] = {
--- spec/requests/api/v1/specializations_spec.rb
+++ spec/requests/api/v1/specializations_spec.rb
@@ -5,8 +5,19 @@ RSpec.describe 'api/v1/specializations', type: :request do
   path '/api/v1/specializations' do
 
     get('list specializations') do
+      tags 'Specializations'
+      produces 'application/json'
       response(200, 'successful') do
-
+        schema type: :array,
+               items: {
+                 type: :object,
+                 properties: {
+                   id: { type: :integer },
+                   name: { type: :string, example: Faker::Name.name },
+                   created_at: { type: :string, format: :datetime, example: Faker::Date.backward(days: 23) },
+                   updated_at: { type: :string, format: :datetime, example: Faker::Date.backward(days: 23) }
+                 }
+               }
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -19,12 +30,14 @@ RSpec.describe 'api/v1/specializations', type: :request do
     end
 
     post('create specialization') do
+      tags 'Create specialization'
+      produces 'application/json'
       response(200, 'successful') do
         consumes 'application/json'
         parameter name: :specialization, in: :body, schema: {
           type: :object,
           properties: {
-            name: { type: :string }
+            name: { type: :string, example: Faker::Name.name }
           },
           required: %w[name]
         }
