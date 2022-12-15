5c11ca94e385e700483160cd0a176208c3ad76e0
usbbush <jeanbienvenusb@gmail.com>
2022-12-15 11:26:28 +0200

    Remove duplicate code

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,11 +15,3 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       expose: ['Authorization']
   end
 end
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
-    resource '*',
-             headers: :any,
-             methods: [:get, :post]
-  end
-end
