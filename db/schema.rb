f6e8e1a9db63fb741b452ed9416ed9dd7fd3b1b1
usbbush <jeanbienvenusb@gmail.com>
2022-12-29 17:36:03 +0200

    Update host link

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_15_092252) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_15_220657) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_092252) do
     t.bigint "freelancer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city", default: "No city"
     t.index ["freelancer_id"], name: "index_reservations_on_freelancer_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
