32c56ae81337597e0788b87d1ee960e68aa7e15e
usbbush <jeanbienvenusb@gmail.com>
2022-12-31 18:53:43 +0200

    Refactor reservation freelancer object

--- app/serializers/reservation_serializer.rb
+++ app/serializers/reservation_serializer.rb
@@ -1,8 +1,10 @@
 class ReservationSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
   attributes :id, :appointment_date, :city, :freelancer
 
   def freelancer
-    object.freelancer
+    freelancer = object.freelancer
+    { **freelancer, **featured_image(freelancer) }
   end
 
   def featured_image(freelancer)
